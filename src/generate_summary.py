215ee2e2f37c132e93a815b5378fbd4ea49722ce
Yusuf Sulehman <yusuf.sulehman@postgrad.manchester.ac.uk>
2024-03-31 16:25:36 +0100

    instance attributes

--- src/generate_summary.py
+++ src/generate_summary.py
@@ -1,2 +1,11 @@
+import requests
+
 class News:
-    pass
+    def __init__(self, list_of_keywords, word_limit, email_frequency, email):
+        self.lst_keywords = list_of_keywords
+        self.word_limit = word_limit
+        self.email_freq = email_frequency
+        self.email = email
+
+    def _get_keyword_articles(self, lst_of_keywords):
+        pass 
