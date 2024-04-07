be0eeb0bfa921e4bcf7676bfce15a803a6e64f7b
Yusuf Sulehman <yusuf.sulehman@postgrad.manchester.ac.uk>
2024-04-07 12:58:18 +0100

    extract news article

--- src/generate_summary.py
+++ src/generate_summary.py
@@ -4,7 +4,8 @@ from datetime import date, timedelta
 
 class News:
     api_key = 'f50463cd13ed4a009e961148e1a90a05'
-    url = 'https://newsapi.org/v2/everything'
+    url = 'https://newsapi.org/v2/everything' #search every article published by over 5,000 different sources in the last 5 years
+    url_breaking = 'https://newsapi.org/v2/top-headlines' #provides live top and breaking headlines
 
     def __init__(self, list_of_keywords: list, word_limit: int, email_frequency: str, email: str):
         self.lst_keywords = list_of_keywords
@@ -23,6 +24,16 @@ class News:
             oldest_article_from = str(todays_date - timedelta(days = 7))
         for word in lst_of_keywords:
             #specify query parameters
-            params = {'apiKey': self.api_key, 'q': word, 'language': 'en', 'from': oldest_article_from, 'pageSize': 2}
+            params = {'apiKey': self.api_key, 'q': word, 'language': 'en', 'from': oldest_article_from, 'pageSize': 1}
 
-            article_info = requests.get(self.url,params=params)
+            article_request = requests.get(self.url,params=params)
+
+            if article_request.status_code != 200:
+                print(f'unsuccessful request, error code: {article_request.status_code} reason: {article_request.reason}')
+            else:
+                article_data = article_request.json()
+
+                #get news article description
+                articles = article_data['articles']
+                for article in articles:
+                    print(article['description'])
