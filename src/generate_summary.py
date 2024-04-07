8481658086429f41fefe969eaa6310d20f435f28
Yusuf Sulehman <yusuf.sulehman@postgrad.manchester.ac.uk>
2024-04-07 13:57:45 +0100

    test cases and handling instance of no search results

--- src/generate_summary.py
+++ src/generate_summary.py
@@ -18,22 +18,36 @@ class News:
     def _get_keyword_articles(self, lst_of_keywords: list, articles_from: str):
         #get 'from' query parameter
         todays_date = date.today()
-        if articles_from is 'daily':
-            oldest_article_from = str(todays_date - timedelta(days = 1))
-        else:
+        if articles_from == 'weekly':
             oldest_article_from = str(todays_date - timedelta(days = 7))
         for word in lst_of_keywords:
             #specify query parameters
-            params = {'apiKey': self.api_key, 'q': word, 'language': 'en', 'from': oldest_article_from, 'pageSize': 1}
+            if articles_from == 'weekly':
+                weekly_params = {'apiKey': self.api_key, 'q': word, 'language': 'en', 'from': oldest_article_from, 'pageSize': 1}
 
-            article_request = requests.get(self.url,params=params)
+                article_request = requests.get(self.url,params=weekly_params)
+            else:
+                daily_params = {'apiKey': self.api_key, 'q': word, 'country': 'gb','pageSize': 1}
+
+                article_request = requests.get(self.url_breaking,params=daily_params)
+                #print(article_request.url)
 
             if article_request.status_code != 200:
                 print(f'unsuccessful request, error code: {article_request.status_code} reason: {article_request.reason}')
             else:
                 article_data = article_request.json()
 
-                #get news article description
-                articles = article_data['articles']
-                for article in articles:
-                    print(article['description'])
+                #check if any results returned
+                total_results = article_data['totalResults']
+                if total_results == 0:
+                    print('No relevant news articles found.')
+
+                else:
+                    #get news article description
+                    articles = article_data['articles']
+                    for article in articles:
+                        article_content = article['content']
+                        article_url = article['url']
+
+if __name__=="__main__":
+    n = News(['United'], 200, 'daily', 'ysulehman@hotmail.com')
