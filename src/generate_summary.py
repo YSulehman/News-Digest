01f92276340f126ff8ddb54cca42355fb34ff455
Yusuf Sulehman <yusuf.sulehman@postgrad.manchester.ac.uk>
2024-04-28 23:15:05 +0100

    write to file

--- src/generate_summary.py
+++ src/generate_summary.py
@@ -4,6 +4,7 @@ from datetime import date, timedelta
 
 #get api-key
 news_api_key = os.environ.get('NEWS_API_KEY')
+news_api_key = news_api_key.strip("'") #had issue with single quotes, it included these from the environment variable.
 
 class News:
     headers = {'X-Api-Key': news_api_key}
@@ -26,14 +27,16 @@ class News:
         for word in lst_of_keywords:
             #specify query parameters
             if articles_from == 'weekly':
-                weekly_params = {'q': word, 'language': 'en', 'from': oldest_article_from, 'pageSize': 1}
-
+                weekly_params = {'q': word, 'language': 'en', 'from': oldest_article_from, 'pageSize': 1, 'apiKey': news_api_key}
+                #print(self.headers['X-Api-Key'])
+                #print(weekly_params['apiKey'])
                 article_request = requests.get(self.url,params=weekly_params, headers=self.headers)
+                #article_request = requests.get(self.url,params=weekly_params)
             else:
                 daily_params = {'q': word, 'country': 'gb','pageSize': 1}
 
                 article_request = requests.get(self.url_breaking,params=daily_params, headers=self.headers)
-            print(article_request.url)
+            #print(article_request.url)
 
             if article_request.status_code != 200:
                 print(f'unsuccessful request, error code: {article_request.status_code} reason: {article_request.reason}')
@@ -48,10 +51,22 @@ class News:
                 else:
                     #get news article description
                     articles = article_data['articles']
+                    #print(len(articles))
                     for article in articles:
-                        article_content = article['content']
+                        title = article['title']
+                        article_content = article['description']
                         article_url = article['url']
-                        print(article_content)
+                        #print(article_content)
+                        print(title)
+                        print(len(article_content))
+                        print(article_url)
+                        self._write_to_file(title, article_content, article_url, './test_article.txt')
+
+    def _write_to_file(self, heading: str, contents: str, article_url: str, file_name: str):
+        with open(file_name, 'w') as f:
+            f.write(heading + '\n\n')
+            f.write(contents + '\n\n')
+            f.write(article_url)
 
 if __name__=="__main__":
     n = News(['United'], 200, 'weekly', 'ysulehman@hotmail.com')
