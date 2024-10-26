13c09e9006d75a56624f95d635b69923b1ae4128
Yusuf Sulehman <yusuf.sulehman@postgrad.manchester.ac.uk>
2024-10-26 13:08:48 +0100

    loading environment variables

--- src/generate_summary.py
+++ src/generate_summary.py
@@ -5,15 +5,18 @@ from openai import OpenAI
 from email.message import EmailMessage
 from datetime import date, timedelta
 
-#get api-key, this was done specifically (?) for windows os. Is it necessary/applicable for linux? 
-news_api_key = os.environ.get('NEWS_API_KEY')
-news_api_key = news_api_key.strip("'") #had issue with single quotes, it included these from the environment variable.
-openai_api_key = os.environ.get('OPENAI_API_KEY')
 
 class News:
+    # load environment variables
+    news_api_key = os.environ.get('NEWS_API_KEY')
+    news_api_key = news_api_key.strip("'") #had issue with single quotes, it included these from the environment variable.
+    openai_api_key = os.environ.get('OPENAI_API_KEY')
+    email_key = os.environ.get('p_email_key')
+
     headers = {'X-Api-Key': news_api_key}
     url = 'https://newsapi.org/v2/everything' #search every article published by over 5,000 different sources in the last 5 years
     url_breaking = 'https://newsapi.org/v2/top-headlines' #provides live top and breaking headlines
+
 
     def __init__(self, list_of_keywords: list, word_limit: int, email_frequency: str, recipient_email: str, sender_email: str):
         self.lst_keywords = list_of_keywords
@@ -21,6 +24,7 @@ class News:
         self.email_freq = email_frequency
         self.recipient_email = recipient_email
         self.sender_email = sender_email
+
 
         #self._get_keyword_articles(self.lst_keywords, self.email_freq)
 
@@ -32,7 +36,7 @@ class News:
         for word in lst_of_keywords:
             #specify query parameters
             if articles_from == 'weekly':
-                weekly_params = {'q': word, 'language': 'en', 'from': oldest_article_from, 'pageSize': 1, 'apiKey': news_api_key}
+                weekly_params = {'q': word, 'language': 'en', 'from': oldest_article_from, 'pageSize': 1, 'apiKey': News.news_api_key}
                 article_request = requests.get(self.url,params=weekly_params, headers=self.headers)
                 #article_request = requests.get(self.url,params=weekly_params)
             else:
@@ -77,7 +81,7 @@ class News:
         """
         Summarise news article using OpenAI's GPT-3.5 model
         """
-        client = OpenAI(api_key=openai_api_key)
+        client = OpenAI(api_key=News.openai_api_key)
 
         response = client.chat.completions.create(
             messages = [
@@ -106,11 +110,10 @@ class News:
             with smtplib.SMTP_SSL('smtp-mail.outlook.com', 587) as server:
                 # make server connection secure
                 server.starttls()
-                server.login(sender_email_address, os.environ.get('EMAIL_PASSWORD'))
+                server.login(sender_email_address, News.email_key)
                 server.send_message(msg)
         except Exception as e:
             print(f'Error sending email: {e}')
 
 
-if __name__=="__main__":
-    n = News(['United'], 200, 'weekly', 'ysulehman@hotmail.com')
+
